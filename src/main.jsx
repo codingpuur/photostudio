0e675f87fc415bb03a74a38e08c6b5cba69efe1f
shubham chaudhari <chaudharysc73342@gmail.com>
2023-07-08 17:39:17 +0530

    logo update

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-<style>
-@import url('https://fonts.googleapis.com/css2?family=Nunito:wght@200&display=swap');
-</style>
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
